35fce8ae89bd62e87cbd9abfd1aab9e8798afd5e
DANIEL ABEDNEGO <daniel.abednego@creditenable.com>
2023-01-02 19:28:56 +0530

    fix date

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import axios from 'axios';
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-const baseUrl = 'https://absensiappwebapi.azurewebsites.net/api/v1/admin/';
-// const baseUrl = 'https://localhost:5001/api/v1/admin/';
+// const baseUrl = 'https://absensiappwebapi.azurewebsites.net/api/v1/admin/';
+const baseUrl = 'https://localhost:5001/api/v1/admin/';
 
 export default function App() {
   const [startDate, setStartDate] = useState(new Date());
@@ -45,9 +45,7 @@ export default function App() {
     let from = new Date(Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), 0, 0, 0));
     let to = new Date(Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), 23, 59, 59));
 
-    await axios.post(baseUrl + "get-data-between-date",
-      { data: { startDate: from, endDate: to }, responseType: 'blob' },
-      options)
+    await axios.post(baseUrl + "get-data-between-date", { dateFrom: from, dateTo: to, responseType: 'blob' }, options)
       .then((response) => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
